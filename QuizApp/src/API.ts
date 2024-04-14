1bfe9fefd0566fea3de3c20196437844b5bd07f1
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-04-14 14:50:36 +0530

    random answers generation done

--- QuizApp/src/API.ts
+++ QuizApp/src/API.ts
@@ -1,3 +1,5 @@
+import { shuffleArray } from "./utils"
+
 export type Question = {
   category : string,
   correct_answer : string,
@@ -18,5 +20,9 @@ export enum Difficult {
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficult) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
   const data = await ((await fetch(endpoint))).json()
-  console.log(data)
+  
+  return data.results.map((question:Question)=>({
+    ...question,
+    answer:shuffleArray([...question.correct_answer,question.incorrect_answer])
+  }))
 }
